fefff7a8ac47047d0d99ca1f6b5ffd2a50869e71
Marek Laasik <marek@rockit.ee>
2017-09-10 15:10:55 +0300

    Modified interface protection level

--- src/Ee.Rockit.LuisClient/Api/ILuisApi.cs
+++ src/Ee.Rockit.LuisClient/Api/ILuisApi.cs
@@ -1,15 +1,11 @@
-﻿using Ee.Rockit.LuisClient.Client;
-using Ee.Rockit.LuisClient.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RestSharp;
-
-namespace Ee.Rockit.LuisClient.Api
+﻿namespace Ee.Rockit.LuisClient.Api
 {
-    interface ILuisApi : IApiAccessor
+    using Ee.Rockit.LuisClient.Client;
+    using Ee.Rockit.LuisClient.Model;
+    using System.Threading.Tasks;
+    using RestSharp;
+
+    public interface ILuisApi : IApiAccessor
     {
         string AppId { get; set; }
 
